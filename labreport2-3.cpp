21be5e873cdeba11ac2eeb20d7a89cd8528ad7cb
Hitu <fjahanhitu@gmail.com>
2026-01-19 22:46:20 +0600

    Update labreport2-3.cpp

--- labreport2-3.cpp
+++ labreport2-3.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class circle
+{
+private:
+    float radius;
+public:
+    void setRadius(float r){
+         radius=r;}
+    float area(){
+         return 3.1416*radius*radius;}
+    float circumference(){
+         return 2*3.1416*radius;}
+};
+int main()
+{
+    circle c;
+    float r;
+
+    cout<<"Enter radius: ";
+    cin>>r;
+
+    c.setRadius(r);
+
+    cout <<"Area of Circle= "<<c.area()<<endl;
+    cout <<"Circumference of Circle= "<<c.circumference()<<endl;
+
+    return 0;
+}
 
